47a8adef39be761fabdb84d9b8a02422788d8b53
Darshan2024 <darshanugale10@gmail.com>
2025-04-06 03:52:54 -0400

    complete working transcriber

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import request
 import flask
 import os
+import sys
 import subprocess
 os.environ['MPLBACKEND'] = 'Agg'
 
@@ -12,19 +13,28 @@ app = flask.Flask(__name__)
 def transcribe():
     args = request.args
     voice_file = args.get('voice_file')
+    transcribed_text = ''
 
     with open('batch.txt', 'w') as batch:
-        batch.write(voice_file+'\n')
-    subprocess.run([
-        "Faster-Whisper-XXL/faster-whisper-xxl",
-        "batch.txt",
-        "-m", "turbo",
-        "--task", "transcribe",
-        "--diarize", "reverb_v2",
-        "-br",
-        "-o", "Transcripts",
-        "-f", "txt", "srt"
-    ])
+        batch.write('/voice/'+voice_file+'\n')
+    process = subprocess.run(
+        [
+            "Faster-Whisper-XXL/faster-whisper-xxl",
+            "batch.txt",
+            "-m", "turbo",
+            "--task", "transcribe",
+            "--diarize", "reverb_v2",
+            "-br",
+            "-o", "/voice",
+            "-f", "txt", "srt"
+        ],
+        capture_output=True)
+    print(process.stdout, file=sys.stderr)
+    with open(f'/voice/{voice_file.split(".")[0]}.txt') as transcribed:
+        for line in transcribed.readline():
+            transcribed_text += line
+    return {"transcribed_text": transcribed_text}
+
 
 if __name__ == '__main__':
-    app.run(debug = True, host = '0.0.0.0', port = 65535) 
+    app.run(debug=True, host='0.0.0.0', port=65535)
